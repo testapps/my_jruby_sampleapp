f842c198220ec35f5397825e57267897ab9163de
Arun Agrawal <arun@fromjaipur.com>
2011-03-28 14:09:42 +0530

    Initial Load.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyJrubyApp::Application.config.secret_token = '044b982af577f5d678dc31f644fb3ab059de9561cf78d1c12c0e0fc9f82f8757e08f5cd6cd1d9e266c6b418a47cf23f54447351e10f76fb4574d539f3f48aef3'
